7cbe677d9ea3964f97038a92d6e9f309c1b40f8e
Kabya55 <kabyakishor017@gmail.com>
2026-06-17 13:30:00 +0600

    auth: add better-auth server configuration

---
+++ src/lib/auth.js
@@ -0,0 +1,38 @@
+import { betterAuth } from "better-auth";
+import { MongoClient } from "mongodb";
+import { mongodbAdapter } from "better-auth/adapters/mongodb";
+
+const client = new MongoClient(process.env.MONGO_DB_URI);
+const db = client.db(process.env.AUTH_DB_URI || "StartupForge");
+
+export const auth = betterAuth({
+
+  baseURL: process.env.BETTER_AUTH_URL,
+
+  database: mongodbAdapter(db, {
+    client,
+  }),
+  emailAndPassword: {
+    enabled: true,
+  },
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    },
+  },
+  user: {
+    additionalFields: {
+      role: {
+        type: "string",
+        defaultValue: "collaborator",
+        input: true,
+      },
+      package: {
+        type: "string",
+        defaultValue: "collaborator_free",
+        input: true,
+      },
+    },
+  },
+});
